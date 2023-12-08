bb0a30a779045391e28fc7ee6cf572dc3e095c1d
reagan <reaganm746@gmail.com>
2023-12-08 13:02:27 +0300

    done

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
-
 class CashRegister:
-  pass
+    def __init__(self):
+        self.items = []
+        self.total = 0
+        self.discount = 0
+        self.last_transaction = 0
+
+    def add_item(self, title, price, quantity=1):
+        item_cost = price * quantity
+        self.total += item_cost
+        self.items.append({"title": title, "price": price, "quantity": quantity})
+        self.last_transaction = item_cost
+
+    def void_last_transaction(self):
+        if self.last_transaction != 0:
+            self.total -= self.last_transaction
+            self.last_transaction = 0
+
+    def apply_discount(self):
+        if self.discount != 0:
+            discount_amount = (self.discount / 100) * self.total
+            self.total -= discount_amount
+            return f"After a {self.discount}% discount, the total comes to ${self.total}."
+        else:
+            return "There is no discount to apply."
+
+
